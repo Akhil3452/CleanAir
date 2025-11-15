7e1533cc638e0e3703058510e2dc6f4c13f5aed3
Akhil <akhilthamatam260@gmail.com>
2025-11-15 12:00:00 +0530

    Login Screen Implemented

--- app/src/main/java/uk/ac/tees/mad/cleanair/ui/screens/auth/AuthBackground.kt
+++ app/src/main/java/uk/ac/tees/mad/cleanair/ui/screens/auth/AuthBackground.kt
@@ -1,2 +1,25 @@
 package uk.ac.tees.mad.cleanair.ui.screens.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import uk.ac.tees.mad.cleanair.ui.theme.DeepSky
+import uk.ac.tees.mad.cleanair.ui.theme.LightSky
+
+@Composable
+fun AuthBackground(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    listOf(LightSky, DeepSky)
+                )
+            )
+    ) {
+        content()
+    }
+}
